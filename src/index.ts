e5c20be67a5292c7bee861a9d9ca7cdd04e01945
“ArslanKibria” <kibriya.mushtaq@ferrum.network>
2023-07-25 17:57:13 +0500

    awsSecretsManager init at index.ts module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 import app from "./app";
+import awsSecretsManager from "./utils/awsSecretsManager";
+
 dotenv.config();
+
+(async () => {
+  await awsSecretsManager();
+})().catch((e) => {
+  console.log(e);
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.info(`Listening to port ${process.env.PORT}`);
